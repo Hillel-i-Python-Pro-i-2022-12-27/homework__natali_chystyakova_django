405e9d90b7118bc305d8e05533b5f47ca71bee01
Natali <natali.dnp.ua@gmail.com>
2023-02-03 18:00:08 +0200

    fix class UsersView

--- apps/first_example/views/users_view.py
+++ apps/first_example/views/users_view.py
@@ -1,22 +1,3 @@
-# from django.views.generic import TemplateView, CreateView
-#
-#
-# from apps.first_example.services.generate_humans import generate_humans, generate_users
-#
-#
-# class HumansView(TemplateView):
-#
-#     template_name = "first_example/humans.html"
-#
-#     def get_context_data(self, amount: int = 7, **kwargs) -> dict:
-#         context_data = super().get_context_data(**kwargs)
-#         context_data["title"] = "Humans"
-#
-#         context_data["humans"] = generate_humans(amount=amount)
-#
-#         return context_data
-
-
 from django.views.generic import TemplateView
 
 from apps.first_example.services.generate_users import generate_users
@@ -30,6 +11,7 @@ class UsersView(TemplateView):
         context_data = super().get_context_data(**kwargs)
         context_data["title"] = "Users"
 
+        context_data["amount"] = amount
         context_data["users"] = generate_users(amount=amount)
 
         return context_data
