be5289833802efa9b537793cf72bbab45d9799c3
Natali <natali.dnp.ua@gmail.com>
2023-02-12 03:19:56 +0200

    fix models +href

--- apps/contacts/models.py
+++ apps/contacts/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Contact(models.Model):
@@ -8,6 +9,9 @@ class Contact(models.Model):
     modified_at = models.DateTimeField(auto_now=True)
 
     is_auto_generated = models.BooleanField(default=False)
+
+    def get_url(self):
+        return reverse("contacts:details", args=[self.id])
 
     def __str__(self) -> str:
         return f"{self.name} - {self.phone}"
