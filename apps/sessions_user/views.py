88ce82da3dd3431aabdcce28d66cb143f74eca86
Natali <natali.dnp.ua@gmail.com>
2023-02-25 14:12:02 +0200

    fix views session_user

--- apps/sessions_user/views.py
+++ apps/sessions_user/views.py
@@ -1,13 +1,14 @@
 from typing import Final
 from datetime import datetime
 
-from faker import Faker
+
 from django.contrib.sessions.backends.cached_db import SessionStore
 from django.views.generic import TemplateView
 
 KEY__COUNT_OF_VISITS: Final[str] = "count_of_visits"
-KEY__EXTRA_DATA: Final[str] = "extra_data"
+# KEY__EXTRA_DATA: Final[str] = "extra_data"
 DATA_NOW: [str] = "data_now"
+NESTED_TIME: [str] = "nested_time"
 
 
 class SessionUserView(TemplateView):
@@ -16,32 +17,21 @@ class SessionUserView(TemplateView):
     def get_context_data(self, **kwargs):
         session: SessionStore = self.request.session
         count_of_visits = session.get(KEY__COUNT_OF_VISITS, 0)
+
         count_of_visits += 1
         data_now = str(datetime.now())
+        nested_time = session.get(NESTED_TIME, 0)
+        session[NESTED_TIME] = nested_time
+        session[NESTED_TIME] = data_now
+
         session[KEY__COUNT_OF_VISITS] = count_of_visits
-        session[DATA_NOW] = data_now
-
-        try:
-            extra_data = session[KEY__EXTRA_DATA]
-        except KeyError:
-            extra_data = {}
-            session[KEY__EXTRA_DATA] = extra_data
-        extra_data["user"] = Faker().name()
-
-        try:
-            nested_data = extra_data[KEY__EXTRA_DATA]
-        except KeyError:
-            nested_data = {}
-            extra_data[KEY__EXTRA_DATA] = nested_data
-        nested_data["user"] = Faker().name()
 
         context = super().get_context_data(**kwargs)
         context["title"] = "Session user"
         context["session_id"] = session.session_key
         context["count_of_visits"] = count_of_visits
-        context["extra_data"] = extra_data
+
         context["data_now"] = data_now
+        context["nested_time"] = nested_time
+
         return context
-
-
-# Create your views here.
